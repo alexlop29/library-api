f18f5ab1dcf05c56f05901ba2f4f1e5e355dec9e
alopez-axiom <alopez@axiomcloud.ai>
2023-10-26 18:00:46 -0400

    Configure Sentry

--- app.ts
+++ app.ts
@@ -3,14 +3,50 @@ NOTE TO SELF: Reminder to update.
 */
 
 import express from "express";
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
+import { SENTRY_DSN } from "./config/environment";
 
 const app = express();
 const EXPRESS_PORT = 3000;
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0,
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 // Sample Route
 app.get("/", (_req, res) => {
   res.send("Hi Alex");
 });
+
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
+
 
 app.listen(EXPRESS_PORT, () => {
   console.log(`Server is running on http://localhost:${EXPRESS_PORT}`);
--- config/environment.ts
+++ config/environment.ts
@@ -3,3 +3,4 @@ dotenv.config();
 
 export const EXPRESS_PORT = process.env.EXPRESS_PORT;
 export const MONGO_DB_URI = process.env.MONGO_DB_URI ?? '';
+export const SENTRY_DSN = process.env.SENTRY_DSN;
