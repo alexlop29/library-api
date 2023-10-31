0118c992f92b5a87818afc0eb0f3bf6a46d0c7b7
alopez-axiom <alopez@axiomcloud.ai>
2023-10-30 20:09:30 -0400

    Build tests for the book controller

--- tests/controllers/book.test.ts
+++ tests/controllers/book.test.ts
@@ -1,20 +1,24 @@
 import { BookController } from "../../controllers/book";
+import { mongoose } from "../setupFile";
 
 const fakeBook = {
-  isbn: '1234567891111',
-  librarianId: '653d47a4c811c0691340e0d4'
+  isbn: "1234567891111",
+  librarianId: "653d47a4c811c0691340e0d4",
 };
 
-const book = new BookController;
+const book = new BookController();
 
-test('retrieves all books', async () => {
-    const getAllBooks = await book.getBooks();
-    expect(getAllBooks).toContain('Successfully retrieved all books');
-    // expect(getAllBooks.returnedBooks).toEqual({
-    //     "_id": "653d47a4c811c0691340e0d4",
-    //     "isbn": 9780062079978,
-    //     "librarianId": "653bdd7b96f4c08936a8316d",
-    //     "__v": 0
-    // })
-    expect(getAllBooks.returnedBooks).toHaveProperty('isbn', expect.assertions(2));
-  });
+test("retrieves all books", async () => {
+  const getAllBooks = await book.getBooks();
+  expect(getAllBooks).toContain("Successfully retrieved all books");
+  // expect(getAllBooks.returnedBooks).toEqual({
+  //     "_id": "653d47a4c811c0691340e0d4",
+  //     "isbn": 9780062079978,
+  //     "librarianId": "653bdd7b96f4c08936a8316d",
+  //     "__v": 0
+  // })
+  expect(getAllBooks.returnedBooks).toHaveProperty(
+    "isbn",
+    expect.assertions(2),
+  );
+});
