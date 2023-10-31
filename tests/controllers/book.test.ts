e557c7f8400806a8ca756f3329916e367a61e3a1
alopez-axiom <alopez@axiomcloud.ai>
2023-10-31 10:58:05 -0400

    Configure jest to use typescript

--- tests/controllers/book.test.ts
+++ tests/controllers/book.test.ts
@@ -1,24 +1,32 @@
-import { BookController } from "../../controllers/book";
+import { BookController, Book } from "../../controllers/book";
 import { mongoose } from "../setupFile";
+import {describe, expect, test} from '@jest/globals';
 
 const fakeBook = {
-  isbn: "1234567891111",
+  isbn: 1234567891111,
   librarianId: "653d47a4c811c0691340e0d4",
 };
 
 const book = new BookController();
 
-test("retrieves all books", async () => {
-  const getAllBooks = await book.getBooks();
-  expect(getAllBooks).toContain("Successfully retrieved all books");
-  // expect(getAllBooks.returnedBooks).toEqual({
-  //     "_id": "653d47a4c811c0691340e0d4",
-  //     "isbn": 9780062079978,
-  //     "librarianId": "653bdd7b96f4c08936a8316d",
-  //     "__v": 0
-  // })
-  expect(getAllBooks.returnedBooks).toHaveProperty(
-    "isbn",
-    expect.assertions(2),
-  );
+test("creates a new book", async () => {
+  const newBook = new Book(fakeBook.isbn, fakeBook.librarianId);
+  const createBook = await book.createBook(newBook);
+  console.log(createBook);
+  expect(createBook).toContain("");
 });
+
+// test("retrieves all books", async () => {
+//   const getAllBooks = await book.getBooks();
+//   expect(getAllBooks).toContain("Successfully retrieved all books");
+//   // expect(getAllBooks.returnedBooks).toEqual({
+//   //     "_id": "653d47a4c811c0691340e0d4",
+//   //     "isbn": 9780062079978,
+//   //     "librarianId": "653bdd7b96f4c08936a8316d",
+//   //     "__v": 0
+//   // })
+//   expect(getAllBooks.returnedBooks).toHaveProperty(
+//     "isbn",
+//     expect.assertions(2),
+//   );
+// });
